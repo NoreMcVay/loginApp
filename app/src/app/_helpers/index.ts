3a16daa3e8b2d20ed8f680916abb97171c4aae47
Noor McVay <38887557+NoreMcVay@users.noreply.github.com>
2019-12-20 22:08:28 +0000

    Add files via upload

---
+++ app/src/app/_helpers/index.ts
@@ -0,0 +1,4 @@
+export * from './error.interceptor';
+export * from './jwt.interceptor';
+// tslint:disable-next-line:eofline
+export * from './fake-backend';
---
+++ app/src/app/app.routing.ts
@@ -0,0 +1,26 @@
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register';
+import { AuthGuard } from './_guards';
+import { NgModule } from '@angular/core';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes, {useHash: true})
+    ],
+         exports: [RouterModule]
+    })
+
+export class AppRoutingModule {
+}
